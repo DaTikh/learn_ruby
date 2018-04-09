a26f3d9295e253f0449efa5b7e7aa778e5b7877c
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-09 21:13:44 +0200

    a degager

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,22 +1,37 @@
 def translate(a)
   vow = ["a", "e", "i", "o", "u", "y"]
   ay = "ay"
+  exception = "qu"
   phrase = []
 
   a.split(" ").each do |w|
-    # print a.split
-    if vow.include? w[0]
-      word = w + ay
-      # phrase << word
-    else
-      w = w.split(//)
-      w.rotate! until vow.include? w[0]
-      word = w.join("") + ay
+
+      # w.split(//)
+    if w.index("qu")
+      popo = w.split
+      longueur = popo.length
+      print longueur
+        print popo
+      popo.rotate! until  "q" == w[longueur-2]
+
+      word = popo.join("") + ay
+
     end
+    # end
+  # if vow.include? w[0]
+  #     word = w + ay
+  #     # phrase << word
+  #   else
+  #     # # w = w.split(//)
+  #     # w.rotate! until vow.include? w[0]
+  #     # word = w.join("") + ay
+  #
+  #   end
+
       phrase << word
   end
   return phrase.join(" ")
 end
 
-# ah = "aghte oputfg"
-# puts translate(ah)
+ah = "quiet"
+puts translate(ah)
