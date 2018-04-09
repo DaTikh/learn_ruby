5751a8ebdea78e6667bc7c6e14246598b6184857
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-09 22:01:15 +0200

    exo 4 fini, début 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,22 +1,30 @@
 def translate(a)
+
   vow = ["a", "e", "i", "o", "u", "y"]
   ay = "ay"
   phrase = []
 
   a.split(" ").each do |w|
-    # print a.split
-    if vow.include? w[0]
+    # -- cas "squ" --
+    if  w.index("squ")
+      w = w.split(//).rotate(3)
+      word = w.join("") + ay
+    # -- cas "qu" --
+    elsif w.index("qu")
+      w = w.split(//).rotate(2)
+      word = w.join("") + ay
+    # -- voyelle --
+    elsif vow.include? w[0]
       word = w + ay
-      # phrase << word
+    # -- consonne rotate jusqu'à voyelle --
     else
       w = w.split(//)
       w.rotate! until vow.include? w[0]
       word = w.join("") + ay
     end
+
       phrase << word
-  end
+   end
+
   return phrase.join(" ")
 end
-
-# ah = "aghte oputfg"
-# puts translate(ah)
