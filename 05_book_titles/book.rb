569d90d9d33c38c4413b1e27f1ba787429c5115d
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-09 23:41:34 +0200

    exo 5-8

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -2,7 +2,19 @@ class Book
   attr_accessor :title
 
   def title=(new)
-    @title = new.capitalize!
+    c = []
+    conjunctions = ["and", "but", "for", "nor", "or", "so", "yet"]
+    prepositions = ["over", "the","a", "an", "in", "of"]
+    new.split(" ").each do |b|
+      if conjunctions.include? b
+        c << b
+      elsif prepositions.include? b
+        c << b
+      else
+        c << b.capitalize!
+      end
+      @title = c.join(" ")
+    end
   end
 
 end
