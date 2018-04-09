a6347d7e234104ca213af5c1403d1d4021246b17
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-09 18:25:49 +0200

    2 consonnes OK

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,23 +1,20 @@
 def translate(a)
   vow = ["a", "e", "i", "o", "u", "y"]
   ay = "ay"
-  word = []
+  phrase = []
 
   a.split(" ").each do |w|
 
     if vow.include? w[0]
       return w + ay
-
     else
       w = w.split(//)
-      word << w.rotate! until vow.include? w[0]
-      puts word
+      w.rotate! until vow.include? w[0]
     end
-puts word.join("") + ay
+      return w.join("") + ay
   end
 
 end
 
-
-ah = "apple banana cherry"
+ah = "testos jrye"
 puts translate(ah)
