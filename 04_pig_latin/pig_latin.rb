018447d22e81d417966d8fdbb696061a54e9ddfa
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-09 17:44:43 +0200

    spec 1 ok

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
-#write your code here
+def translate(a)
+  vow = ["a", "e", "i", "o", "u", "y"]
+  ay = "ay"
+  word = []
+
+  a.split(" ").each do |w|
+
+    if vow.include? w[0]
+      return w + ay
+
+    else
+      w = w.split(//)
+      word << w.rotate! until vow.include? w[0]
+      puts word
+    end
+puts word.join("") + ay
+  end
+
+end
+
+
+ah = "apple banana cherry"
+puts translate(ah)
