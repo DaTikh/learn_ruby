e83fe0edc2d67d63e751fec0115a35b4ede5422a
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-10 00:08:49 +0200

    upcase on I - test is_a? Integer

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -4,11 +4,20 @@ class Book
   def title=(new)
     c = []
     conjunctions = ["and", "but", "for", "nor", "or", "so", "yet"]
-    prepositions = ["over", "the","a", "an", "in", "of"]
+    prepositions = ["over", "in", "of"]
+    articles = ["the", "a", "an"]
+    # new = new.to_s
     new.split(" ").each do |b|
       if conjunctions.include? b
         c << b
       elsif prepositions.include? b
+        c << b
+      elsif articles.include? b
+        c << b
+      elsif b.length == 1 && b == "i"
+        c << b.upcase!
+      elsif b.is_a? Integer
+        # print b
         c << b
       else
         c << b.capitalize!
@@ -18,3 +27,7 @@ class Book
   end
 
 end
+
+
+puts @book = Book.new
+puts @book.title = "I Knew When I was 20"
